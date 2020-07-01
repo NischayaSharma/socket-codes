b9fe6cb7499e08318f725a93658f5a1742dbeece
NischayaSharma <nischaya1703@gmail.com>
2020-07-01 16:18:00 +0530

    fix: Updating the script for Python3

--- socket-codes/client/src/client.py
+++ socket-codes/client/src/client.py
@@ -3,15 +3,15 @@ import sys;
 
 try:
     s = socket.socket();
-    s.connect(('18.219.110.96',12345));
+    s.connect(('127.0.0.1',12345));
     while True:
-        str = raw_input("S: ");
+        str = input("S: ");
         s.send(str.encode());
         if(str == "Bye" or str == "bye"):
             break;
-        print "N:",s.recv(1024).decode();
+        print ("N:",s.recv(1024).decode());
     s.close();
 except KeyboardInterrupt:
-    print "Closing Connection and freeing the port."
+    print ("Closing Connection and freeing the port.")
     s.close();
     sys.exit();
--- socket-codes/server/src/server.py
+++ socket-codes/server/src/server.py
@@ -1,32 +1,29 @@
 import socket;
 import sys;
-import thread;
+import _thread;
 
-try:
-    s = socket.socket();
-    port = 12345;
-    s.bind(('', port));
-    s.listen(5);
-    i = 0;
 
-    def main(c,numOfThread):
-        while True:
-            rcvdData = c.recv(1024).decode();
-            print "Thread:", numOfThread+1, "S:", rcvdData;
-            sendData = raw_input("N: ");
-            c.send(sendData.encode());
-            if(sendData == "Bye" or sendData == "bye"):
-                break;
-        c.close();
+s = socket.socket();
+port = 12345;
+s.bind(('', port));
+s.listen(5);
+i = 0;
 
+def main(c,numOfThread):
     while True:
-        print "Server Script started";
-        c, addr = s.accept();
-        print "Socket Up and running with a connection from",addr;
-        thread.start_new_thread(main,(c,i,));
-        i+=1;
-
-except KeyboardInterrupt:
-    print "\nClosing Connection and freeing the port."
+        rcvdData = c.recv(1024).decode();
+        print ("Thread:", numOfThread+1, "S:", rcvdData);
+        sendData = input("N: ");
+        c.send(sendData.encode());
+        if(sendData == "Bye" or sendData == "bye"):
+            break;
     c.close();
-    sys.exit();
+
+while True:
+    print ("Server Script started");
+    c, addr = s.accept();
+    print ("Socket Up and running with a connection from",addr);
+    _thread.start_new_thread(main,(c,i,));
+    i+=1;
+
+
